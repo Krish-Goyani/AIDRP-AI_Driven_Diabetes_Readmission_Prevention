5a54c4ef01a6a69adecd91b6f3fae7845cd5eac5
Krish-Goyani <goyani@duck.com>
2024-02-13 12:29:55 +0530

    os module removed

--- src/AIDRP/utils/common.py
+++ src/AIDRP/utils/common.py
@@ -1,5 +1,4 @@
 # we are using some functions repeatedly we wil store it here
-import os
 from box.exceptions import BoxValueError
 import yaml
 from src.AIDRP.logging import logger
@@ -47,7 +46,8 @@ def create_directories(path_to_directories: list, verbose=True):
         ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
-        os.makedirs(path, exist_ok=True)
+        path = Path(path)
+        path.mkdir(parents=True, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
 
